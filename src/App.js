386385a5a1bbe42164b60b91712cceb9fe8cba06
ZeynepE1 <81360699+ZeynepE1@users.noreply.github.com>
2021-10-19 15:50:57 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import counterpart from 'counterpart';
+import Translate from 'react-translate-component';
+import en from './language/en';
+import tr from './language/tr';
 
-function App() {
+counterpart.registerTranslations('en', en);
+counterpart.registerTranslations('tr', tr);
+counterpart.setLocale('en');
+
+const Link = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Translate
+      content={props.content}
+      component="a"
+      href="//google.com"
+      target="_blank"
+    />
+  )
+}
+
+class App extends Component {
+
+  state = {
+    lang: 'en'
+  }
+
+  onLangChange = (e) => {
+    this.setState({lang: e.target.value});
+    counterpart.setLocale(e.target.value);
+  }
+
+  render() {
+    const link = <Link content="link"/>;
+    return (
+      <div className="App">
+
+        <select value={this.state.lang} onChange={this.onLangChange}>
+          <option value="en">EN</option>
+          <option value="tr">TR</option>
+        </select>
+
+        <Translate content="title" component="h1" className="class"/>
+
+        <Translate content="copy.p1" component="p" unsafe={true}/>
+
+        <Translate content="copy.p2" component="p" with={{ link }}/>
+
+        <Translate component="input" type="text" attributes={{placeholder: 'placeholder'}}/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
